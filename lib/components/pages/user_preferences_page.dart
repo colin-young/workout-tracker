c6117807767d3bb1ef7d36168af477bc1af9df69
Colin Young <colin.young.todo@gmail.com>
2024-05-08 22:43:26 -0400

    Fix scroll in preferences page

--- lib/components/pages/user_preferences_page.dart
+++ lib/components/pages/user_preferences_page.dart
@@ -2,18 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:workout_tracker/components/common/custom_scaffold.dart';
 import 'package:workout_tracker/components/pages/user_preferences/user_preferences_editor.dart';
-import 'package:workout_tracker/data/user_preferences_state.dart';
+import 'package:workout_tracker/data/repositories/user_preferences_repository.dart';
 
-class UserPreferencesPage extends ConsumerWidget with UserPreferencesState {
+class UserPreferencesPage extends ConsumerWidget {
   const UserPreferencesPage({
     super.key,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return CustomScaffold(
-      title: const Text('User preferences'),
-      body: UserPreferencesEditor(preferences: userPreferences(ref)),
-    );
+    final userPreferencesResult = ref.watch(getUserPreferencesProvider);
+
+    return switch (userPreferencesResult) {
+      AsyncValue(:final value?) => CustomScaffold(
+          title: const Text('User preferences'),
+          body: UserPreferencesEditor(preferences: value),
+        ),
+      _ => const CircularProgressIndicator(),
+    };
   }
 }
